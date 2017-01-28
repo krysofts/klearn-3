a31652d6c46902e7f8fbe4239e926f998cc4fbdd
Kryse <Kryse@krysoft.ro>
2017-01-29 01:40:14 +0200

    Added tank AI controller base

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI not posessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI possessing: %s"), *ControlledTank->GetName());
+	}
+	UE_LOG(LogTemp, Warning, TEXT("AIController Beginplay"));
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
